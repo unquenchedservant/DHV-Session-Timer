32d81b1a6fc5e20dc2639611947b1d126f02ec04
Jon Thorne <jon@jonthorne.com>
2025-01-05 18:33:40 -0500

    added some comments

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import time
 from playsound import playsound
 
+#keeping this for later, for adjustability
 def play_sound_after_delay(sound_file, delay):
     time.sleep(delay)
     playsound(sound_file)
 
+#default is 10 minutes, with a temp change at 6 minutes and
+# another at 8 minutes
+
+# plan is to have the timer be a little bit more...flowy.
+# Will probably make PyQT timer for this later to allow
+# for a settings page
 if __name__ == "__main__":
     sound_file = 'asset/ding.mp3'
-    
     time.sleep(60)
     print("1 minute has passed")
     time.sleep(60)
