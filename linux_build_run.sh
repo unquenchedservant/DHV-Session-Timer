65d275018cf8ccf6789c8907dfe4046c04623668
Jonathan Thorne <jon@jonthorne.com>
2025-06-28 22:58:26 -0400

    update build files

--- linux_build_run.sh
+++ linux_build_run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR"
 source venv/bin/activate
-python3 -m PyInstaller src/DHVSessionTimer.spec
-sudo cp src/dist/DHVSessionTimer /usr/bin/DHVSessionTimer
+killall DHVSessionTimer
+cd src
+python3 -m PyInstaller DHVSessionTimer.spec
+sudo cp dist/DHVSessionTimer /usr/bin/DHVSessionTimer
 DHVSessionTimer
