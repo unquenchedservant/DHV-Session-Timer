08e6d861e59fc806d47d246a3e9c4047c84e4d0f
Jonathan Thorne <jon@jonthorne.com>
2025-06-28 22:46:50 -0400

    Update action, update build scripts, update release file

--- linux_build_run.sh
+++ linux_build_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source venv/bin/activate
-python3 -m PyInstaller DHVSessionTimer.spec
-sudo cp dist/DHVSessionTimer /usr/bin/DHVSessionTimer
+python3 -m PyInstaller src/DHVSessionTimer.spec
+sudo cp src/dist/DHVSessionTimer /usr/bin/DHVSessionTimer
 DHVSessionTimer
--- mac_build.sh
+++ mac_build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-rm -rf /Users/jthorne/Developer/DHV-Session-Timer/dist/dmg
-rm -rf /Users/jthorne/Developer/DHV-Session-Timer/dist/DHVSessionTimer.app
-rm  /Users/jthorne/Developer/DHV-Session-Timer/dist/DHVSessionTimer.dmg
-cd /Users/jthorne/Developer/DHV-Session-Timer
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+rm -rf "$SCRIPT_DIR/src/dist/dmg"
+rm -rf "$SCRIPT_DIR/src/dist/DHVSessionTimer.app"
+rm  "$SCRIPT_DIR/src/dist/DHVSessionTimer.dmg"
+cd "$SCRIPT_DIR"
 source venv/bin/activate
-python3 -m PyInstaller DHVSessionTimer.spec
-cd /Users/jthorne/Developer/DHV-Session-Timer/dist
+python3 -m PyInstaller src/DHVSessionTimer.spec
+cd "$SCRIPT_DIR/src/dist"
 mkdir dmg
 cp -r DHVSessionTimer.app dmg/
-create-dmg --volname "DHVSessionTimer" --window-pos 200 120 --window-size 600 300 --hide-extension "DHVSessionTimer.app" --app-drop-link 425 120 /Users/jthorne/Developer/DHV-Session-Timer/dist/DHVSessionTimer.dmg /Users/jthorne/Developer/DHV-Session-Timer/dist/dmg/
+create-dmg --volname "DHVSessionTimer" --window-pos 200 120 --window-size 600 300 --hide-extension "DHVSessionTimer.app" --app-drop-link 425 120 "$SCRIPT_DIR/dist/DHVSessionTimer.dmg" "$SCRIPT_DIR/src/dist/dmg/"
